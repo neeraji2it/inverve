a04c6d44d55b1937c4086bebfcc327c1997ac7d1
rails <rails@rails-desktop.(none)>
2014-11-14 10:23:54 +0530

    my cart and destroy of cart and number of products in cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,4 +2,18 @@ class CartsController < ApplicationController
 	def show
     @cart = current_cart
   end
+  def index
+    @cart = current_cart
+    @products = current_cart.line_items
+    end
+    def destroy
+    @cart = LineItem.find(params[:id])
+    if @cart.destroy
+      if !current_cart.line_items.present?
+        current_cart.destroy
+        session[:cart_id] = nil
+      end
+      redirect_to carts_path
+    end
+  end
 end
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,13 +1,14 @@
 class LineItemsController < ApplicationController
 	def create
-    @product = Product.find(params[:product_id])
-@cart = LineItem.find_by_product_id_and_cart_id(@product.id,current_cart)
-if @cart.present?
-@cart.update_attribute(:quantity, @cart.quantity+1)
-else
-    @line_item = LineItem.create!(:cart => current_cart, :product => @product, :quantity => 1, :unit_price => @product.price)
-end
-    flash[:notice] = "Added #{@product.name} to cart."
-    redirect_to cart_path(current_cart)
-  end
+		@product = Product.find(params[:product_id])
+		@cart = LineItem.find_by_product_id_and_cart_id(@product.id,current_cart)
+		if @cart.present?
+			@cart.update_attribute(:quantity, @cart.quantity+1)
+		else
+			@line_item = LineItem.create!(:cart => current_cart, :product => @product, :quantity => 1, :unit_price => @product.price)
+		end
+		flash[:notice] = "Added #{@product.name} to cart."
+		redirect_to cart_path(current_cart)
+	end
+
 end
