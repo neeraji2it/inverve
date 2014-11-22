18f51e73e8d0a86df63889b695cd52916b3de9c9
rails <rails@rails-desktop.(none)>
2014-11-22 12:06:44 +0530

    properly managed if else in cart session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,8 +26,8 @@ def current_cart
     if session[:cart_id]
       @current_cart ||= Cart.find(session[:cart_id])
       session[:cart_id] = nil if @current_cart.purchased_at
-    end
-    if session[:cart_id].nil?
+    else
+     session[:cart_id].nil?
       @current_cart = Cart.create!
       session[:cart_id] = @current_cart.id
     end
