073848e5bfd4c29bfaee75e0bdef4310092d85c2
rails <rails@rails>
2015-01-08 19:51:01 +0530

    config for deployment

---
+++ config/deploy/production.rb
@@ -0,0 +1,6 @@
+set :domain, "107.155.113.112"
+set :rails_env, 'production'
+set :gitrepo, "git@github.com:neeraji2it/homefurnishing.git"
+set :deploydir, "/var/www/rosarahomes.com/public_html"
+set :user, "shivraj"
+set :use_sudo, false
