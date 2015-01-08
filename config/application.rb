0950a1e20178b3c2eec0e944a95fad5ef66b0537
rails <rails@rails-desktop.(none)>
2015-01-08 15:41:48 +0530

    final design is implementing

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module Furnitureapp
     config.assets.precompile += [
       'hella-tite.css'
     ]
+    config.assets.initialize_on_precompile = false
     config.i18n.enforce_available_locales = true
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
