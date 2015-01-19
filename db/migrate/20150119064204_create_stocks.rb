d9b931ac8ccfef5f492b7d4abbbb55d652108034
rails <rails@rails>
2015-01-19 16:05:56 +0530

    stockmanagement feature with quantity updation

--- db/migrate/20150119064204_create_stocks.rb
+++
@@ -1,9 +0,0 @@
-class CreateStocks < ActiveRecord::Migration
-  def change
-    create_table :stocks do |t|
-      t.references :products, :index => true
-      t.integer :quantity
-      t.timestamps
-    end
-  end
-end
--- spec/factories/stocks.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :stock do
-    
-  end
-
-end
