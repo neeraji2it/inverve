a44298a4a789d56256fdab65de085e11fd8c61eb
rails <rails@rails-desktop.(none)>
2015-01-23 18:50:24 +0530

    footer content contactus, address, copyright text changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
-def sortable(column, name = nil)
-name||= column.nameize
-css_class = (column == sort_column) ? "current &crarr;
-#{sort_direction}" : nil
-direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-link_to name, params.merge(:sort => column, :direction => direction, :page => nil), {:class => css_class}
-end
+	def sortable(column, name = nil)
+		name||= column.nameize
+		css_class = (column == sort_column) ? "current &crarr;
+		#{sort_direction}" : nil
+		direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+		link_to name, params.merge(:sort => column, :direction => direction, :page => nil), {:class => css_class}
+	end
+
+
 end
