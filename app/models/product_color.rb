1fe9fdd62883a86437c3afa1a7c1ee365d507199
rails <rails@rails>
2015-01-26 17:34:26 +0530

    added images gallery in admin product show

--- app/models/product_color.rb
+++
@@ -1,4 +0,0 @@
-class ProductColor < ActiveRecord::Base
-  belongs_to :product
-  belongs_to :color
-end
--- spec/factories/product_colors.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :product_color do
-    product_id 1
-color_id 1
-  end
-
-end
