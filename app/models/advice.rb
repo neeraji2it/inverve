22542be8dfc7acea3af8b1b36c44be5254fc788e
rails <rails@rails>
2015-01-29 17:15:09 +0530

    thumb created

--- app/models/advice.rb
+++ app/models/advice.rb
@@ -1,5 +1,5 @@
 class Advice < ActiveRecord::Base
-  has_attached_file :avatar, :styles => { :thumb => "300x300", :small => "60x60"}, :default_url => "/assets/:style/missing.png"
+  has_attached_file :avatar, :styles => { :thumb => "700x460", :small => "60x60"}, :default_url => "/assets/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates :title, :avatar, :description, :presence => true
   validates :title, uniqueness: true
