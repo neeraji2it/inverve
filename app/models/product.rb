45ef2fc1a88f65cc560607ba87c60a8f6a3913b6
rails <rails@rails>
2015-02-05 12:48:46 +0530

    implemented size of the product and displayed it on admin index, show, home listing, details and popup pages

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
    has_many :images, :dependent => :destroy
    has_many :line_items, :dependent => :destroy
    belongs_to :sub_category
-   validates :name, :description, :price, :sub_category_id, :color,  presence: true 
+   validates :name, :description, :price, :sub_category_id, :color, :product_size,  presence: true 
    validates :name, :uniqueness => {:case_sensitive => false}
    validates :price, :numericality => {:only_float => true}
    accepts_nested_attributes_for :images, reject_if: :all_blank, :allow_destroy => true
