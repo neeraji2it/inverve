0541589d46d20564b461d86eabe549fefe243c20
rails <rails@rails>
2015-04-22 11:38:11 +0530

    production env set for new server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-set :domain, "107.155.113.112"
+set :domain, "63.142.250.68"
 set :rails_env, 'production'
 set :gitrepo, "git@github.com:neeraji2it/homefurnishing.git"
 set :deploydir, "/var/www/rosarahomes.com/public_html"
-set :user, "shivraj"
+set :user, "neeraj"
 set :use_sudo, false
