b90a24493e27332db9cdacd0ac0a0177cbab7b83
rails <rails@rails>
2015-04-25 14:11:59 +0530

    set root user in deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -61,17 +61,17 @@
 #   }
 
 
-role :app, %w{neeraj@63.142.250.68}
-role :web, %w{neeraj@63.142.250.68}
-role :db, %w{neeraj@63.142.250.68}
+role :app, %w{root@63.142.250.68}
+role :web, %w{root@63.142.250.68}
+role :db, %w{root@63.142.250.68}
 # Extended Server Syntax
 # ======================
 # This can be used to drop a more detailed server definition into the
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
-server '63.142.250.68', user: 'neeraj', roles: %w{web app db}
+server '63.142.250.68', user: 'root', roles: %w{web app db}
 set :domain, '63.142.250.68'
 set :deploy_to, '/var/www/html/rosarahomes.com/public_html'
 set :branch, 'master'
-set :deploy_user, 'neeraj'
+set :deploy_user, 'root'
 set :log_level, :debug
