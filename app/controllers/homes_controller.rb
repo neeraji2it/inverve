05bc2a75e2cf9b390df8467653a55a5572fa2e63
neeraji2it <neeraji2it@gmail.com>
2015-09-01 14:41:44 +0530

    product test

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -45,7 +45,7 @@ def single_product
   @product = Product.find(params[:id])
   @images = @product.images
   @sub_category = @product.sub_category.present? ? (@product.sub_category) : @product.category
-  @similars = @sub_category.products.where.not(id: @product.id)
+#  @similars = @sub_category.products.where.not(id: @product.id)
 end
 
 def guide
